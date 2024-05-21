13dedc9737059d1480bf65226f3110b1f391409b
Maxim Lapis <maxim.lapis@gmx.de>
2024-05-21 11:55:38 +0200

    set root + included rules

--- app/controllers/bullshits_controller.rb
+++ app/controllers/bullshits_controller.rb
@@ -62,6 +62,9 @@ class BullshitsController < ApplicationController
   def basic_auth
   end
 
+  def rules
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_bullshit
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :bullshits do
     collection do
       get  "basic_auth"
+      get  "rules"
     end
   end
   devise_for :users
@@ -12,5 +13,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "bullshits#index"
 end
